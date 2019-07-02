4eadf874dd97b3ca84ff03eea04f7ce032d4cead
Ludovic Nicaise <ludovic.nicaise@dom-jvs.local>
2019-07-02 16:34:38 +0200

    refactor BamHeader

--- ludo/src/BamHeader.js
+++ ludo/src/BamHeader.js
@@ -2,22 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Header} from 'react-native-elements';
 
-const leftComponent = cb => ({
-  icon: "undo",
-  color: "#fff",
+const iconColor = "#fff";
+const iconType = 'ionicon';
+
+const iconComponent = (icon, cb) => ({
+  icon,
+  type: iconType,
+  color: iconColor,
   onPress: cb
 });
 
-const rightComponent = cb => ({
-  icon: "person",
-  color: "#fff",
-  onPress: cb
-});
+const leftComponent = cb => iconComponent("ios-log-out", cb);
+
+const rightComponent = cb => iconComponent("ios-person", cb);
 
 const centerComponent = title =>
   React.isValidElement(title)
     ? title
-    : { text: title, style: { color: "#fff" } };
+    : { text: title, style: { color: iconColor } };
 
 const BamHeader = props => {
   const { title, bamAction } = props;
