790967cc7b3e437d426f5f88f462f2e0318713a2
Ludovic Nicaise <ludovic.nicaise@dom-jvs.local>
2019-07-02 16:53:04 +0200

    update BamHeader

--- ludo/src/BamHeader.js
+++ ludo/src/BamHeader.js
@@ -2,9 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Header} from 'react-native-elements';
 
+/**
+ * Color - white
+ */
 const iconColor = "#fff";
+/**
+ * Type - ionicon
+ */
 const iconType = 'ionicon';
 
+/**
+ * Generique icon component
+ * @param {string} icon name of icon
+ * @param {function} cb callback
+ */
 const iconComponent = (icon, cb) => ({
   icon,
   type: iconType,
@@ -12,18 +23,38 @@ const iconComponent = (icon, cb) => ({
   onPress: cb
 });
 
+/**
+ * Left icon component (log out)
+ * @param {function} cb callback
+ */
 const leftComponent = cb => iconComponent("ios-log-out", cb);
 
+/**
+ * Right icon component (profile)
+ * @param {function} cb callback
+ */
 const rightComponent = cb => iconComponent("ios-person", cb);
 
+/**
+ * Title component
+ * @param {string | React.ReactElement} title title of header
+ */
 const centerComponent = title =>
   React.isValidElement(title)
     ? title
     : { text: title, style: { color: iconColor } };
 
+/**
+ * BamHeader component
+ * @param {object} props props of BamHeader
+ */
 const BamHeader = props => {
   const { title, bamAction } = props;
 
+  /**
+   * Call bamAction with action on press
+   * @param {string} action name of action
+   */
   const onPress = (action) => {
     bamAction({
       action
@@ -39,11 +70,17 @@ const BamHeader = props => {
   );
 };
 
+/**
+ * Props Types
+ */
 BamHeader.propTypes = {
   title: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
   bamAction: PropTypes.func.isRequired
 };
 
+/**
+ * Default props
+ */
 BamHeader.defaultProps = {
   title: ""
 };
