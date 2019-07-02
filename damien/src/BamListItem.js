69aa6d892753b0e146c1ca02470f5c423746b09b
Damien COEVOET <damien.coevoet@jvs.fr>
2019-07-02 16:49:11 +0200

    [WIP] BamListItem - Add username and image

--- damien/src/BamListItem.js
+++ damien/src/BamListItem.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 
 class BamListItem extends Component {
 
@@ -13,32 +13,45 @@ class BamListItem extends Component {
 
     render() {
         const {
-            message,
+            message, username, avatar
         } = this.props;
+
+        const imageLink = avatar || "https://images4.persgroep.net/rcs/iQoMRKfl6Q9Ta9AKQEmhUWgnoXo/diocontent/75305151/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.9";
 
 
         return (
+
             <View  style={styles.container}>
-               <Text style={{textAlignVertical: 'top',textAlign: "left",}}>{message}</Text>
-            </View>
+            <Image
+              style={styles.image}
+              source={{uri: imageLink}} /> 
+            <Text style={styles.text}>[{username}] - {message}</Text>
+          </View>
         );
     }
 }
 
 BamListItem.defaultProps = {
-    message: null,
+    avatar: null
 };
 
 BamListItem.propTypes = {
     message: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    avatar: PropTypes.string,
 };
 
 const styles = StyleSheet.create({
     container: {
+        width: '100%',
+        flexDirection:'row', 
+        flexWrap:'wrap',
       textAlign: "left",
       backgroundColor: '#fff',
       color: '#000',
     },
+    image: {width: 50, height: 50, borderRadius: 400/ 2},
+    text: {textAlignVertical: "center"}
   });
 
 export default BamListItem;
