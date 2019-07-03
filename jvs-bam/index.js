6e09757fffedf1236622312de3a8e81f03a5f16b
freezeburger <renaud.dubuis@decryptage.net>
2019-07-03 10:22:59 +0200

    jvs-bam link component

--- jvs-bam/index.js
+++ jvs-bam/index.js
@@ -1,3 +1,16 @@
+import BamAvatar from './bam-avatar/index'
+import BamField from './bam-field/index'
+import {BamGrumpyCat} from './bam-grumpy-cat/index'
+import {BamHeader} from './bam-header/index'
+import {BamList} from './bam-list/index'
+import {BamListItem} from './bam-list-item/index'
+import {BamLogoButton} from './bam-logo-button/index'
 
-
-// export { MyComponent };
+export { 
+    BamAvatar, 
+    BamField, 
+    BamGrumpyCat, 
+    BamHeader,
+    BamList,
+    BamListItem,
+    BamLogoButton}
